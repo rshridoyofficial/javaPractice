0fd554dbcc392188af2a14d2ad39b3376fc58f16
Akibul Hasan Hridoy <akibulhasanhridoywp@gmail.com>
2025-09-13 16:03:42 +0600

    first class in java

---
+++ Main.java
@@ -0,0 +1,21 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("HEllo World");
+
+//        dara types in Java:
+        //premetive data typs :
+        //byte - 1 [-128 to 127]
+        //short - 2
+        //int - 4 1,2,3,
+        //long - 8 I
+        //float - 4 3.14
+        //double - 8
+        //char - 2 a,b,c,d
+        //boolean - 1 true/false
+
+        //Non-Premetive Data Types
+
+    }
+}
